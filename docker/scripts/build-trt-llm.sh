3e77472e8b63356e5361566d846b2d002937e233
z-zawhtet-a <z.zawhtet.a@ieee.org>
2024-05-11 08:49:16 +0000

    add more target cuda arch

--- docker/scripts/build-trt-llm.sh
+++ docker/scripts/build-trt-llm.sh
@@ -3,7 +3,7 @@
 export ENV=${ENV:-/etc/shinit_v2}
 source $ENV
 
-CUDA_ARCH="${CUDA_ARCH:-89-real;90-real}"
+CUDA_ARCH="${CUDA_ARCH:-75-real;86-real;89-real;90-real}"
 
 cd /root/TensorRT-LLM
 python3 scripts/build_wheel.py --clean --cuda_architectures "$CUDA_ARCH" --trt_root /usr/local/tensorrt
