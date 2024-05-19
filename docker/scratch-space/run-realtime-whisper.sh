05aa4b76526f693966b20115f41b2ea643a9ad2e
z-zawhtet-a <z.zawhtet.a@ieee.org>
2024-05-19 10:01:19 +0000

    change to multilingual

--- docker/scratch-space/run-realtime-whisper.sh
+++ docker/scratch-space/run-realtime-whisper.sh
@@ -5,5 +5,5 @@ test -f /etc/shinit_v2 && source /etc/shinit_v2
 cd /root/RealtimeWhisper
 
 echo "Running RealtimeWhisper..."
-exec python3 run_server.py -p 8080 -trt /root/scratch-space/models/whisper_model
+exec python3 run_server.py -p 8080 -trt /root/scratch-space/models/whisper_model -m
 
